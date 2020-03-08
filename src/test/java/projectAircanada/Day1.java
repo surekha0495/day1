2faca6d4dd64ee21144192b5b4e9ff642365c347
surekha0495 <surekha.0495@gmail.com>
2020-03-07 21:49:36 -0800

    i done with my login code

--- src/test/java/projectAircanada/Day1.java
+++ src/test/java/projectAircanada/Day1.java
@@ -1,9 +1,10 @@
-package projectAircanada;
+	package projectAircanada;
 
 public class Day1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubs
+		
+		System.out.println("login code");
 
 	}
 
