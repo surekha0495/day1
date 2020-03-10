3b024cb5a320d1327bd6770ad658119e417340bf
surekha0495 <surekha.0495@gmail.com>
2020-03-09 18:26:33 -0700

    new line added in chandu

--- src/test/java/projectAircanada/Day1.java
+++ src/test/java/projectAircanada/Day1.java
@@ -2,10 +2,10 @@
 
 public class Day1 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("login code");
-
+System.out.println("hello");
+System.out.println("welcome");
 	}
 
 }
